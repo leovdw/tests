7b0694986c964c61459546a3bb9b670b970d5735
Leo vdw <leopold.vonderweid@aramisauto.com>
2021-03-16 10:40:15 +0100

    initial commit

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+// ===========================================================
+// ======================= FizzBuzz ==========================
+// ===========================================================
+
+// Cet exercice consiste a faire une fonction fizzBuzz, avec comme règle les suivantes :
+// La fonction prend un nombre en paramètre
+// La fonction doit :
+// - Si le nombre donné en paramètre est un multiple de 3 on doit voir dans notre console "Fizz"
+// - Si le nombre donné en paramètre est un multiple de 5 on doit voir dans notre console "Buzz"
+// - Si le nombre donné en paramètre est un multiple de 3 et de 5 on doit voir dans notre console "FizzBuzz"
+// Sinon on doit voir le nombre que la fonction est entrain d'examiner.
+
+
+
+// Code Here
+
+zz
